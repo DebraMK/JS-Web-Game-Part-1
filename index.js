8d2241be728891ca60cc1e9dbe9e80746c950648
DebraMK <dmkochrn@outlook.com>
2022-03-23 21:10:37 -0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgSrc, fixedLeft, fixedBottom, isItem=false){
+    let item = document.createElement('img')
+    item.src = imgSrc
+    item.style.position = 'fixed'
+    item.style.left = fixedLeft
+    item.style.bottom = fixedBottom
+    document.body.append(item) 
+    if (isItem){
+        item.addEventListener('dblclick', 
+        function(){
+            item.remove()
+        })}
+    }
+//still images
+newImage('assets/green-character.gif', '100px', '100px', false)
+
+newImage('assets/pine-tree.png', '450px', '200px', false)
+
+newImage('assets/tree.png', '200px', '300px', false)
+
+newImage('assets/pillar.png', '350px', '100px', false)
+
+newImage('assets/crate.png', '150px', '200px', false)
+
+newImage('assets/well.png', '500px', '425px', false)
+//Retrievable objects
+newImage('assets/sword.png', '500px', '405px', true)
+
+newImage('assets/sheild.png', '165px', '185px', true)
+
+newImage('assets/staff.png', '600px', '100px', true)
+
+//background
+
+document.body.style.backgroundImage = "url('./assets/grass.png')";
+  
